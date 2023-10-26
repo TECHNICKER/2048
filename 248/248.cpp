8053d23677bb7baad990971d0dff4794d22ae09a
Student <Student@PC-SE5113-14>
2023-10-26 08:49:51 +0200

    starrt

--- 248/248.cpp
+++ 248/248.cpp
@@ -6,6 +6,26 @@ int gametrix[4][4] = {	{0, 0, 0, 0},
 						{0, 0, 0, 0},
 					 };
 
+class Block {
+
+	private:
+
+		int value = 0;
+
+	public:
+
+		Block()
+		{
+			//generator
+			;
+		}
+
+		void merge()
+		{
+			;
+		}
+};
+
 int main()
 {
 	return 0;
