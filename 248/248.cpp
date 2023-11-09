18e3e217828f4ba86524adbbaf0402b75f12c33e
Student <Student@PC-SE5113-14>
2023-11-09 08:51:21 +0100

    keys, debounce

--- 248/248.cpp
+++ 248/248.cpp
@@ -1,6 +1,9 @@
 ﻿#include "248.h"
+#include <Windows.h>
 
-int gametrix[4][4] = {	{0, 0, 0, 0},
+
+int gametrix[4][4] = {
+						{0, 0, 0, 0},
 						{0, 0, 0, 0},
 						{0, 0, 0, 0},
 						{0, 0, 0, 0},
@@ -28,5 +31,41 @@ class Block {
 
 int main()
 {
+	bool up_prev = false;
+	bool down_prev = false;
+	bool left_prev = false;
+	bool right_prev = false;
+
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+	{
+		if (up_prev == false)
+		{
+			//do stuff
+			up_prev = true;
+		}
+	} 
+	else 
+	{
+		up_prev = false;
+	}
+
+	/*if ((GetAsyncKeyState(VK_DOWN) & 0x8000) == true)
+	{
+		;// Do stuff
+	}
+
+	if ((GetAsyncKeyState(VK_LEFT) & 0x8000) == true)
+	{
+		;// Do stuff
+	}
+
+	if ((GetAsyncKeyState(VK_RIGHT) & 0x8000) == true)
+	{
+		;// Do stuff
+	}*/
+
+
+
 	return 0;
 }
