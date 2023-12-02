0e3362af305a1219cae1ec7d2f9ab674e16575e8
TECHNICKER <logrik@email.cz>
2023-12-02 19:45:47 +0100

    colourz :3 & font sizing

--- 248/248.cpp
+++ 248/248.cpp
@@ -1,20 +1,41 @@
 ﻿#include "248.h"
 #include <Windows.h>
 #include <Math.h>
-#include <stdlib.h.>
+#include <stdlib.h>
+
 
 
 int gametrix[4][4] = {
-						{2, 0, 2, 2},
-						{0, 2, 2, 2},
-						{0, 2, 0, 2},
-						{2, 0, 0, 2},
+						{2, 4, 8, 16},
+						{32, 64, 128, 256},
+						{512, 1024, 2048, 0},
+						{0, 0, 0, 0},
 					 };
 
-//find movable blocks in desired direction -> return list of pairs of coords
-//find if said blocks are mergible with the closest block in the direction of play ->
-//??return list of pairs of values, a = further most taken coord, b = further most free coord if not mergible with a or a if mergible
+//find movable blocks in desired ith the closest block in the direction of play ->
+//??return list of pairs of values, adirection -> return list of pairs of coords
+//find if said blocks are mergible w = further most taken coord, b = further most free coord if not mergible with a or a if mergible
 //merge said blocks
+
+inline void setFontSize(int a, int b)
+
+{
+
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	PCONSOLE_FONT_INFOEX lpConsoleCurrentFontEx = new CONSOLE_FONT_INFOEX();
+
+	lpConsoleCurrentFontEx->cbSize = sizeof(CONSOLE_FONT_INFOEX);
+
+	GetCurrentConsoleFontEx(hStdOut, 0, lpConsoleCurrentFontEx);
+
+	lpConsoleCurrentFontEx->dwFontSize.X = a;
+
+	lpConsoleCurrentFontEx->dwFontSize.Y = b;
+
+	SetCurrentConsoleFontEx(hStdOut, 0, lpConsoleCurrentFontEx);
+
+}
 
 void draw(void)
 {
@@ -38,7 +59,93 @@ void draw(void)
 	{
 		for (int x = 0; x < 4; x++)
 		{
-			printf("%d", gametrix[y][x]);
+			switch (gametrix[y][x])
+			{
+			case 0:
+
+				printf("\033[0m");
+				printf("%d", gametrix[y][x]);
+
+				break;
+
+			case 2:
+				
+				printf("\033[1;33m");
+				printf("%d", gametrix[y][x]);
+
+				break;
+
+			case 4:
+				
+				printf("\033[0;33m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 8:
+				
+				printf("\033[1;32m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 16:
+				
+				printf("\033[0;32m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 32:
+				
+				printf("\033[1;36m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 64:
+				
+				printf("\033[1;34m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 128:
+				
+				printf("\033[0;34m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 256:
+				
+				printf("\033[1;35m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 512:
+
+				printf("\033[0;35m");
+				printf("%d", gametrix[y][x]);
+
+				break;
+
+			case 1024:
+
+				printf("\033[1;31m");
+				printf("%d", gametrix[y][x]);
+				
+				break;
+
+			case 2048:
+
+				printf("\033[0;31m");
+				printf("%d", gametrix[y][x]);
+
+				break;
+			
+			}
 
 			(gametrix[y][x] > 0) ? (curr_tile_length = floor(log10(abs(gametrix[y][x]))) + 1) : (curr_tile_length = 1);
 
@@ -81,7 +188,7 @@ void merge(char dir)
 		}
 		//
 
-		//MERGE
+		/*//MERGE
 		for (int y = 3; y >= 0; y--)
 		{
 			for (int x = 0; x < 4; x++)
@@ -110,7 +217,7 @@ void merge(char dir)
 				}
 			}
 		}*/
-
+		/*
 		//UPDATE GAME MATRIX FROM MERGED TILES MATRIX
 		for (int y = 0; y < 4; y++)
 		{
@@ -141,7 +248,7 @@ void merge(char dir)
 					}
 				}
 			}
-		}
+		}*/
 
 		break;
 
@@ -164,7 +271,7 @@ void merge(char dir)
 		}
 		//
 
-		//MERGE
+		/*//MERGE
 		for (int y = 0; y < 4; y++)
 		{
 			for (int x = 0; x < 4; x++)
@@ -208,15 +315,50 @@ void merge(char dir)
 					}
 				}
 			}
-		}
+		}*/
 
 		break;
 
 	case 'L':
 
+		//MOVE
+		for (int i = 0; i < 3; i++)
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 4; y++)
+				{
+					if (gametrix[y][x] == 0)
+					{
+						gametrix[y][x] = gametrix[y][x + 1];
+						gametrix[y][x + 1] = 0;
+					}
+				}
+			}
+		}
+		//
+		
+
 		break;
 
 	case 'R':
+
+		//MOVE
+		for (int i = 0; i < 3; i++)
+		{
+			for (int x = 3; x > 0; x--)
+			{
+				for (int y = 0; y < 4; y++)
+				{
+					if (gametrix[y][x] == 0)
+					{
+						gametrix[y][x] = gametrix[y][x - 1];
+						gametrix[y][x - 1] = 0;
+					}
+				}
+			}
+		}
+		//
 
 		break;
 	}
@@ -230,6 +372,8 @@ int main()
 	bool down_prev = false;
 	bool left_prev = false;
 	bool right_prev = false;
+
+	setFontSize(25, 25);
 
 	draw();
 
