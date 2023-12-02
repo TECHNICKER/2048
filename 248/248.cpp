781b3a077ab080a0ef5d64b4400b980bc471bf52
TECHNICKER <logrik@email.cz>
2023-12-02 17:45:40 +0100

    merging up

--- 248/248.cpp
+++ 248/248.cpp
@@ -5,32 +5,11 @@
 
 
 int gametrix[4][4] = {
-						{1, 2, 3, 4},
-						{2, 2, 0, 1},
-						{3, 0, 356, 0},
-						{4, 1, 0, 4},
+						{0, 2, 4, 0},
+						{0, 2, 4, 2},
+						{0, 2, 0, 3},
+						{2, 2, 0, 0},
 					 };
-
-class Block {
-
-	private:
-
-		int value = 0;
-
-	public:
-
-		Block()
-		{
-			//generate a block with less than 100 % chance, change block start value limit according to the highest block alive
-			;
-		}
-
-		void merge()
-		{
-			//generate a new block (at the coordinate of the one being merged to - this prolly in main loop), make it the combination of values of the two dying blocks
-			;
-		}
-};
 
 //find movable blocks in desired direction -> return list of pairs of coords
 //find if said blocks are mergible with the closest block in the direction of play ->
@@ -43,31 +22,137 @@ void draw(void)
 	int curr_tile_length = 0;
 	int scale = 0;
 
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			(largest_tile < gametrix[j][i]) ? (largest_tile = gametrix[j][i]) : (largest_tile = largest_tile);
-		}
-	}
-
-	(largest_tile > 0) ? (scale = floor(log10(abs(largest_tile))) + 1) : (scale = 0);
+	system("cls");
 
 	for (int y = 0; y < 4; y++)
 	{
 		for (int x = 0; x < 4; x++)
 		{
-			printf("%d", gametrix[x][y]);
+			if (largest_tile < gametrix[y][x]) { largest_tile = gametrix[y][x]; }
+		}
+	}
 
-			(gametrix[x][y] > 0) ? (curr_tile_length = floor(log10(abs(gametrix[x][y]))) + 1) : (curr_tile_length = 1);
+	(largest_tile > 0) ? (scale = floor(log10(abs(largest_tile))) + 1) : (scale = 1);
 
-			for (int k = 0; k < (scale - curr_tile_length + 1); k++)
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			printf("%d", gametrix[y][x]);
+
+			(gametrix[y][x] > 0) ? (curr_tile_length = floor(log10(abs(gametrix[y][x]))) + 1) : (curr_tile_length = 1);
+
+			for (int spacer = 0; spacer < (scale - curr_tile_length + 1); spacer++)
 			{
 				printf(" ");
 			}
 		}
 		printf("\n");
 	}
+}
+
+void merge(char dir)
+{
+	int temptrix[4][4] = {
+							{0, 0, 0, 0},
+							{0, 0, 0, 0},
+							{0, 0, 0, 0},
+							{0, 0, 0, 0},
+						 };
+
+	switch (dir)
+	{
+	case 'U':
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					if (gametrix[y][x] == 0)
+					{
+						gametrix[y][x] = gametrix[y + 1][x];
+						gametrix[y + 1][x] = 0;
+					}
+				}
+			}
+		}
+
+		for (int y = 3; y >= 0; y--)
+		{
+			for (int x = 0; x < 4; x++)
+			{
+				if (gametrix[y][x] == gametrix[y - 1][x] && (gametrix[y][x] * 2 != temptrix[y][x]))
+				{
+					temptrix[y - 1][x] = gametrix[y][x] * 2;
+					temptrix[y][x] = -1;
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					if (temptrix[y][x] == 0)
+					{
+						temptrix[y][x] = temptrix[y + 1][x];
+						temptrix[y + 1][x] = 0;
+					}
+				}
+			}
+		}
+
+		for (int y = 0; y < 4; y++)
+		{
+			for (int x = 0; x < 4; x++)
+			{
+				if (temptrix[y][x] != 0)
+				{
+					gametrix[y][x] = temptrix[y][x];
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					if (gametrix[y][x] == -1)
+					{
+						if (y == 3)
+						{
+							gametrix[y][x] = 0;
+						}
+						gametrix[y][x] = gametrix[y + 1][x];
+						gametrix[y + 1][x] = 0;
+					}
+				}
+			}
+		}
+
+		break;
+
+	case 'D':
+
+	
+		break;
+
+	case 'L':
+
+		break;
+
+	case 'R':
+
+		break;
+	}
+
+	
 }
 
 int main()
@@ -77,17 +162,17 @@ int main()
 	bool left_prev = false;
 	bool right_prev = false;
 
+	draw();
+
 	while (1)
 	{
 		if (GetKeyState(VK_UP) & 0x8000)
 		{
 			if (up_prev == false)
 			{
-				system("cls");
 
+				merge('U');
 				draw();
-
-				//printf(" up ");
 
 				up_prev = true;
 			}
@@ -102,7 +187,7 @@ int main()
 			if (down_prev == false)
 			{
 				system("cls");
-				printf(" down ");
+				printf("%d", gametrix[2][1]);
 
 				down_prev = true;
 			}
